ff760d581234a6b1c9aa0f91448d81e0b6366eee
Henrik <henrik@haf.se>
2012-03-05 10:05:45 +0100

    id and the semantic id function seem to work in F# together with a plotter that plots the ID graphically

---
+++ F#/ITC/ITC.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module ITC
+
+type Id = Zero | One | Tuple of Id * Id
+//let (|Zero|) = 0
+//let (|One|)  = 1
+
+let zero x = 0.0
+let one x =
+  match x with
+  | _ when x >= 0.0 && x < 1.0 -> 1.0
+  | _ -> 0.0
+
+let x2 x = 2.0 * x
+let rec semfun id =
+  match id with 
+  | Zero -> zero
+  | One  -> one
+  | Tuple(i', i'') -> (fun x -> (semfun i')(x2 x) + (semfun i'')(x2 x - 1.0))
+
---
+++ F#/ITC/Script.fsx
@@ -0,0 +1,38 @@
+﻿// This file is a script that can be executed with the F# Interactive.  
+// It can be used to explore and test the library project.
+// Note that script files will not be part of the project build.
+#r "../packages/Unquote.2.1.0/lib/net40/Unquote.dll"
+#r "../packages/xunit.1.9.0.1566/lib/xunit.dll"
+#r "System.Windows.Forms.DataVisualization"
+
+open System.Windows.Forms
+open System.Windows.Forms.DataVisualization.Charting
+
+type LineChartForm( title, xs : float seq ) =
+    inherit Form( Text=title )
+
+    let chart = new Chart(Dock=DockStyle.Fill)
+    let area = new ChartArea(Name="Semantic ICTs")
+    let series = new Series()
+    do series.ChartType <- SeriesChartType.Line
+    do xs |> Seq.iter (series.Points.Add >> ignore)
+    do series.ChartArea <- "Semantic ICTs"
+    do chart.Series.Add( series )
+    do chart.ChartAreas.Add(area)
+    do base.Controls.Add( chart )
+
+#load "ITC.fs"
+open ITC
+open Swensen.Unquote
+open Xunit
+
+let the_id = Tuple(One, Tuple(Zero, One))
+let plotter = semfun the_id
+let the_floats = seq { for x in 0.0 .. 100.0 do yield x/100.0 }
+                 |> Seq.map plotter
+
+let form = new LineChartForm("Semantic ICTs", the_floats)
+form.Show()
+//[<Fact>]
+//let ``initial state`` =
+//  
